9714c9b1ee44757241692bc4f55f630f77ce8749
Daksh pratap <daksh.pratap_cs21@gla.ac.in>
2025-03-01 22:47:49 +0530

    Add Logger to Log Messages

--- src/main/java/com/example/SpringIntro/SpringIntroApplication.java
+++ src/main/java/com/example/SpringIntro/SpringIntroApplication.java
@@ -1,13 +1,23 @@
 package com.example.SpringIntro;
 
+import com.example.SpringIntro.Components.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIntroApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(SpringIntroApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntroApplication.class, args);
+		logger.info("Starting Spring Boot Application...");
+		ApplicationContext context = SpringApplication.run(SpringIntroApplication.class, args);
+		logger.debug("Checking Context:{}",context.getBean(DemoBean .class));
+
+		logger.debug("\n*** Example using @Autowire annotation on property ***");
 	}
+
 
 }
